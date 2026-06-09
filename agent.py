816bdc640f43a9a3dafc0b478c7c60d9414554bd
viniciusvilelaa <ninobez@icloud.com>
2026-06-09 12:08:31 -0300

    Feat(agent): Inicializado agente de IA utilizando algoritimo de HUNT e TARGET, Belief State e Mapa de conhecimento

---
+++ agent.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class NavalAgent:
+    def __init__(self , board_size=10):
+        #Definindo o tamanho do tabuleiro
+        self.board_size = board_size
+
+        #Inicializando mapa de conhecimento
+        self.knowledge_map = np.zeros((self.board_size,self.board_size), dtype=int)
+        
+        #Inicializando mapa de probabilidade
+        self.belief_state = np.ones((self.board_size, self.board_size), dtype=float)
+
+        #Alimentando o belief_state para o padrao xadrez. Aumenta a probabilidade para 2 casas intercaladas ja que o minimo de
+        #casas utilizadas por um navio é 2
+        for row in range(self.board_size):
+            for col in range(self.board_size):
+                if(row + col) % 2 == 0:
+                    self.belief_state[row, col] = 2.0
+        
+        #Lista de tiros dados
+        self.shots_fired = set()
+
+        # Metricas de perfomace do agente
+        self.total_shots = 0
+        self.total_hits = 0
+        self.total_misses = 0
+
+        #Controle do modo do agente HUNT ou TARGET
+        self._mode = "HUNT"
+        self._target_queue = []
+        self._active_hits = []
