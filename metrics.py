20805413e00b4264d98b260951d5773b21e0c5eb
Joaremio <joaremiorevoredo@gmail.com>
2026-06-10 14:23:09 -0300

    feat: adiciona metodo para salvar dados da partida em historico

--- metrics.py
+++ metrics.py
@@ -15,4 +15,23 @@ class Metrics:
         elif result == "MISS":
             self.total_misses += 1
 
-    
+    def end_game(self):
+        #Salva os dados da partida atual no histórico
+        if self.total_shots == 0:
+            accuracy = 0.0
+        else:
+            accuracy = self.total_hits / self.total_shots
+
+        partida = {
+            "total_shots": self.total_shots,
+            "total_hits": self.total_hits,
+            "total_misses": self.total_misses,
+            "accuracy": round(accuracy, 3)
+        }
+
+        self.history.append(partida)
+
+        # Reseta para a próxima partida
+        self.total_shots = 0
+        self.total_hits = 0
+        self.total_misses = 0
